00c5db9c6dc2b1eca37fab2daf76e7571311d6a9
carlosveg <carlosvegapro@gmail.com>
2022-12-23 19:07:34 -0600

    Refactorizando para agregar el perceptron

---
+++ ruido/ruidomixto.py
@@ -0,0 +1,36 @@
+import numpy as np
+import cv2
+import random
+from tkinter import filedialog 
+#funcion para la sal
+def sp_noise(image,prob):
+    output=np.zeros(image.shape,np.uint8)
+    thres = 1 -prob
+    for i in range(image.shape[0]):
+        for j in range (image.shape[1]):
+            rdn=random.random()
+            if rdn < prob:
+            
+                output[i][j]=0
+            elif rdn>thres:
+                output[i][j]=255
+            else:
+                output[i][j]=image[i][j]
+
+    return output
+
+
+imagen = filedialog.askopenfilename()
+dir_img = imagen
+fra=cv2.imread(imagen)
+# Cambiar a escala de grises
+gr=cv2.cvtColor(fra, cv2.COLOR_BGR2GRAY)
+# Abrir imagen
+cv2.imshow('imagen original',gr)
+
+k=cv2.waitKey(0)
+
+nois=sp_noise(gr,0.25)
+
+cv2.imshow('imagen con ruido',nois)
+k=cv2.waitKey(0)
