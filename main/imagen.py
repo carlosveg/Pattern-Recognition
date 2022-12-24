4650904555135c63911cb218474f0e111f7c0586
carlosveg <carlosvegapro@gmail.com>
2022-12-23 19:06:39 -0600

    Refactorizando para agregar el perceptron

---
+++ main/imagen.py
@@ -0,0 +1,41 @@
+import cv2
+import numpy as np
+
+
+class Imagen():
+    def __init__(self, nombre, tipo, clasificador):
+        self._imagen = cv2.imread(nombre, tipo)
+        self.__clasificador = clasificador
+
+    def abrir(self):
+        cv2.imshow("imagen", self._imagen)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    def show_image(self):
+        cv2.namedWindow("imagen")
+        cv2.setMouseCallback(
+            "imagen", self.clasificador_clic, param=self._imagen)
+
+        while True:
+            cv2.imshow("imagen", self._imagen)
+            # Para salir del bucle presionar la tecla Esc la cual tiene el valor ASCII = 27
+            # de lo contrario la ventana se seguira abriendo sin fin
+            k = cv2.waitKey(1) & 0xFF
+            if k == 27:
+                break
+
+        cv2.destroyAllWindows()
+
+    def clasificador_clic(self, event, x, y, flags, param):
+        if (event == 1):
+            print(f"{x},{y}: {param[y,x][::-1]}")
+            self.__clasificador.classify([param[y, x]])
+
+    def valor_pixel(self, x, y):
+        # Devolvemos el valor rgb de en formato [r,g,b] del pixel[x,y]
+        # print(f"Valores RGB: {self._imagen[y, x][::-1]}")
+        return self._imagen[y, x][::-1]
+
+    def obtener_canales(self):
+        return cv2.split(self._imagen)
