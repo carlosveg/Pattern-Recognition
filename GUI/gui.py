cef94b322fe45a523a195c50f3de6d5993bf139e
carlosveg <carlosvegapro@gmail.com>
2022-12-30 00:38:45 -0600

    I'm almost done with the interface

--- GUI/gui.py
+++
@@ -1,9 +0,0 @@
-from tkinter import *
-from tkinter import filedialog
-# este pedo es porque en python global no esta el paquete pero en el virtualenv si
-from PIL import Image, ImageTk
-
-
-class GUI():
-    def __init__(self) -> None:
-        pass
